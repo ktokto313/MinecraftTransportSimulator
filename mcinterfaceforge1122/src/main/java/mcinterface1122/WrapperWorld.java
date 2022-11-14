d3fa5c71bbcca9476eba5a1bf71b35ccf417b425
ktokto313 <57142191+ktokto313@users.noreply.github.com>
2022-11-14 22:44:42 +0700

    New Explosion code

--- mcinterfaceforge1122/src/main/java/mcinterface1122/WrapperWorld.java
+++ mcinterfaceforge1122/src/main/java/mcinterface1122/WrapperWorld.java
@@ -2,14 +2,12 @@ package mcinterface1122;
 
 import java.io.File;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import com.google.gson.internal.bind.JsonTreeReader;
 import minecrafttransportsimulator.baseclasses.BoundingBox;
 import minecrafttransportsimulator.baseclasses.Damage;
 import minecrafttransportsimulator.baseclasses.Point3D;
@@ -47,6 +45,7 @@ import net.minecraft.block.BlockSlab;
 import net.minecraft.block.IGrowable;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.enchantment.EnchantmentProtection;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EntityLivingBase;
@@ -62,15 +61,10 @@ import net.minecraft.item.ItemDye;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompressedStreamTools;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.NonNullList;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.RayTraceResult;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.*;
+import net.minecraft.util.math.*;
 import net.minecraft.world.EnumSkyBlock;
+import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 import net.minecraftforge.common.IPlantable;
 import net.minecraftforge.common.MinecraftForge;
@@ -855,7 +849,10 @@ public class WrapperWorld extends AWrapperWorld {
 
     @Override
     public void spawnExplosion(Point3D location, double strength, boolean flames) {
-        world.newExplosion(null, location.x, location.y, location.z, (float) strength, flames, ConfigSystem.settings.general.blockBreakage.value);
+        Explosion explosion = new ExplosionTemp(this.world, null, location.x, location.y, location.z,(float) strength, flames, ConfigSystem.settings.general.blockBreakage.value);
+        if (net.minecraftforge.event.ForgeEventFactory.onExplosionStart(this.world, explosion)) return;
+        explosion.doExplosionA();
+        explosion.doExplosionB(true);
     }
 
     /**
@@ -948,6 +945,138 @@ public class WrapperWorld extends AWrapperWorld {
                 entity.remove();
             }
             worldWrappers.remove(world);
+        }
+    }
+}
+
+class ExplosionTemp extends Explosion {
+    private float size;
+    private World world;
+    private double x;
+    private double y;
+    private double z;
+    private Entity exploder;
+
+    public ExplosionTemp(World worldIn, Entity entityIn, double x, double y, double z, float size, boolean causesFire, boolean damagesTerrain)
+    {
+        super(worldIn, entityIn, x, y, z, size, causesFire, damagesTerrain);
+        this.size = size;
+        this.world = worldIn;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.exploder = super.getExplosivePlacedBy();
+    }
+
+    @Override
+    public void doExplosionA()
+    {
+        Set<BlockPos> set = Sets.<BlockPos>newHashSet();
+        int i = (int) (size * 1.3)+1;
+
+        for (int j = 0; j < i; ++j)
+        {
+            for (int k = 0; k < i; ++k)
+            {
+                for (int l = 0; l < i; ++l)
+                {
+                    if (j == 0 || j == i-1 || k == 0 || k == i-1 || l == 0 || l == i-1)
+                    {
+                        double d0 = (double)((float)j / (float)i * 2.0F - 1.0F);
+                        double d1 = (double)((float)k / (float)i * 2.0F - 1.0F);
+                        double d2 = (double)((float)l / (float)i * 2.0F - 1.0F);
+                        double d3 = Math.sqrt(d0 * d0 + d1 * d1 + d2 * d2);
+                        d0 = d0 / d3;
+                        d1 = d1 / d3;
+                        d2 = d2 / d3;
+                        float f = this.size * (0.7F + this.world.rand.nextFloat() * 0.6F);
+                        double d4 = this.x;
+                        double d6 = this.y;
+                        double d8 = this.z;
+
+                        for (float f1 = 0.3F; f > 0.0F; f -= 0.22500001F)
+                        {
+                            BlockPos blockpos = new BlockPos(d4, d6, d8);
+                            IBlockState iblockstate = this.world.getBlockState(blockpos);
+
+                            if (iblockstate.getMaterial() != Material.AIR)
+                            {
+                                float f2 = this.exploder != null ? this.exploder.getExplosionResistance(this, this.world, blockpos, iblockstate) : iblockstate.getBlock().getExplosionResistance(world, blockpos, (Entity)null, this);
+                                f -= (f2 + 0.3F) * 0.3F;
+                            }
+
+                            if (f > 0.0F && (this.exploder == null || this.exploder.canExplosionDestroyBlock(this, this.world, blockpos, iblockstate, f)))
+                            {
+                                set.add(blockpos);
+                            }
+
+                            d4 += d0 * 0.30000001192092896D;
+                            d6 += d1 * 0.30000001192092896D;
+                            d8 += d2 * 0.30000001192092896D;
+                        }
+                    }
+                }
+            }
+        }
+
+        this.getAffectedBlockPositions().addAll(set);
+        float f3 = this.size * 2.0F;
+        int k1 = MathHelper.floor(this.x - (double)f3 - 1.0D);
+        int l1 = MathHelper.floor(this.x + (double)f3 + 1.0D);
+        int i2 = MathHelper.floor(this.y - (double)f3 - 1.0D);
+        int i1 = MathHelper.floor(this.y + (double)f3 + 1.0D);
+        int j2 = MathHelper.floor(this.z - (double)f3 - 1.0D);
+        int j1 = MathHelper.floor(this.z + (double)f3 + 1.0D);
+        List<Entity> list = this.world.getEntitiesWithinAABBExcludingEntity(this.exploder, new AxisAlignedBB((double)k1, (double)i2, (double)j2, (double)l1, (double)i1, (double)j1));
+        net.minecraftforge.event.ForgeEventFactory.onExplosionDetonate(this.world, this, list, f3);
+        Vec3d vec3d = new Vec3d(this.x, this.y, this.z);
+
+        for (int k2 = 0; k2 < list.size(); ++k2)
+        {
+            Entity entity = list.get(k2);
+
+            if (!entity.isImmuneToExplosions())
+            {
+                double d12 = entity.getDistance(this.x, this.y, this.z) / (double)f3;
+
+                if (d12 <= 1.0D)
+                {
+                    double d5 = entity.posX - this.x;
+                    double d7 = entity.posY + (double)entity.getEyeHeight() - this.y;
+                    double d9 = entity.posZ - this.z;
+                    double d13 = (double)MathHelper.sqrt(d5 * d5 + d7 * d7 + d9 * d9);
+
+                    if (d13 != 0.0D)
+                    {
+                        d5 = d5 / d13;
+                        d7 = d7 / d13;
+                        d9 = d9 / d13;
+                        double d14 = (double)this.world.getBlockDensity(vec3d, entity.getEntityBoundingBox());
+                        double d10 = (1.0D - d12) * d14;
+                        entity.attackEntityFrom(DamageSource.causeExplosionDamage(this), (float)((int)((d10 * d10 + d10) / 2.0D * 7.0D * (double)f3 + 1.0D)));
+                        double d11 = d10;
+
+                        if (entity instanceof EntityLivingBase)
+                        {
+                            d11 = EnchantmentProtection.getBlastDamageReduction((EntityLivingBase)entity, d10);
+                        }
+
+                        entity.motionX += d5 * d11;
+                        entity.motionY += d7 * d11;
+                        entity.motionZ += d9 * d11;
+
+                        if (entity instanceof EntityPlayer)
+                        {
+                            EntityPlayer entityplayer = (EntityPlayer)entity;
+
+                            if (!entityplayer.isSpectator() && (!entityplayer.isCreative() || !entityplayer.capabilities.isFlying))
+                            {
+                                this.getPlayerKnockbackMap().put(entityplayer, new Vec3d(d5 * d10, d7 * d10, d9 * d10));
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
